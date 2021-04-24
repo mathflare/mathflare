81b779b832d7544f968351120de7cea45636545a
dk.raw <hello@dkatsios.tk>
2021-04-24 14:04:40 +0300

    Add files via upload

---
+++ js/prightside.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $("#button").click(function (e) {
+        e.preventDefault();
+        var hypo = $("#hypo").val();
+        var side = $("#side").val();
+        var result = Math.sqrt((hypo * hypo) - (side * side));
+        if (hypo <= 0 || side <= 0) {
+            $("#output").text("Please enter a valid value");
+            $("#hypo").val(1);
+            $("#side").val(1);
+        }
+        else if (side >= hypo) {
+            $("#output").text("The hypotenuse cannot be smaller than or equal to the right side");
+            $("#hypo").val(1);
+            $("#side").val(1);
+        }
+        else {
+            $("#output").text("The other side of the right triangle is " + result + " cm");
+            $("#hypo").val(1);
+            $("#side").val(1);
+        }
+    });
+    $(".show-formula").click(function () {
+        $("#formula").toggle(500);
+    });
+});
