a3026306d82e08845e3b2a0f96c57c6acd46cef5
dk.raw <hello@dkatsios.tk>
2021-04-25 13:17:07 +0300

    Add files via upload

---
+++ js/ctemp.js
@@ -0,0 +1,18 @@
+﻿function temperatureConverter(source, valNum) {
+    valNum = parseFloat(valNum);
+    var inputFahrenheit = document.getElementById("inputFahrenheit");
+    var inputCelsius = document.getElementById("inputCelsius");
+    var inputKelvin = document.getElementById("inputKelvin");
+    if (source == "inputFahrenheit") {
+        inputCelsius.value = ((valNum - 32) / 1.8).toFixed(2);
+        inputKelvin.value = (((valNum - 32) / 1.8) + 273.15).toFixed(2);
+    }
+    if (source == "inputCelsius") {
+        inputFahrenheit.value = ((valNum * 1.8) + 32).toFixed(2);
+        inputKelvin.value = ((valNum) + 273.15).toFixed(2);
+    }
+    if (source == "inputKelvin") {
+        inputFahrenheit.value = (((valNum - 273.15) * 1.8) + 32).toFixed(2);
+        inputCelsius.value = ((valNum) - 273.15).toFixed(2);
+    }
+}
