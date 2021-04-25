66bd0691d5aa7f955533403e8d45440b69ef9dac
dk.raw <hello@dkatsios.tk>
2021-04-25 15:12:23 +0300

    Add files via upload

---
+++ js/cspeed.js
@@ -0,0 +1,27 @@
+﻿function speedConverter(source, valNum) {
+    valNum = parseFloat(valNum);
+    var inputMPH = document.getElementById("inputMPH");
+    var inputKPH = document.getElementById("inputKPH");
+    var inputKnots = document.getElementById("inputKnots");
+    var inputMach = document.getElementById("inputMach");
+    if (source == "inputMPH") {
+        inputKPH.value = (valNum * 1.609344).toFixed(2);
+        inputKnots.value = (valNum / 1.150779).toFixed(2);
+        inputMach.value = (valNum / 761.207).toFixed(4);
+    }
+    if (source == "inputKPH") {
+        inputMPH.value = (valNum / 1.609344).toFixed(2);
+        inputKnots.value = (valNum / 1.852).toFixed(2);
+        inputMach.value = (valNum / 1225.044).toFixed(5);
+    }
+    if (source == "inputKnots") {
+        inputMPH.value = (valNum * 1.150779).toFixed(2);
+        inputKPH.value = (valNum * 1.852).toFixed(2);
+        inputMach.value = (valNum / 661.4708).toFixed(4);
+    }
+    if (source == "inputMach") {
+        inputMPH.value = (valNum * 761.207).toFixed();
+        inputKPH.value = (valNum * 1225.044).toFixed();
+        inputKnots.value = (valNum * 661.4708).toFixed();
+    }
+}
