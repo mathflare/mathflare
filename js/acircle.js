6a64b0a5c50c8702072c8cbd8c12553b03314f52
dk-raw <hello@dkatsios.tk>
2021-04-26 13:46:04 +0300

    cahnged var to const and the message

--- js/acircle.js
+++ js/acircle.js
@@ -1,14 +1,14 @@
 ﻿$(document).ready(function () {
     $("#button").click(calculation = (e) => {
         e.preventDefault();
-        var radius = $("#radius").val();
-        var result = (radius * radius) * 3.14;
+        const radius = $("#radius").val();
+        const result = (radius * radius) * 3.14;
         if (radius <= 0) {
             $("#output").text("Please enter a valid value");
             $("#radius").val(1);
         }
         else {
-            $("#output").text("The area of the circle is ~" + result + " cm^2");
+            $("#output").text("The area of the circle is ~" + result + " cm^2 (π = 3.14)");
             $("#radius").val(1);
         }
     });
