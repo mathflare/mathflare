114f5124cf7378376ae27b24209a0f7060c8a90c
dk-raw <hello@dkatsios.tk>
2021-04-26 19:03:58 +0300

    More PWA stuff

--- sw.js
+++ sw.js
@@ -5,3 +5,18 @@ self.addEventListener('install', async event => {
   self.addEventListener('fetch', async event => {
     console.log('fetch event')
   });
+  const cacheName = 'pwa-conf-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './app.js',
+  './Styles/Site.css',
+  './Styles/Site.css',
+  './Content/bootstrap.css',
+  './Scripts/bootstrap.js',
+  './Scripts/jquery-3.0.0.js'
+];
+self.addEventListener('install', async event => {
+    const cache = await caches.open(cacheName); 
+    await cache.addAll(staticAssets); 
+  });
