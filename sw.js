c5bbfbd456e55325bb99e4813ab5decf638049cb
dk-raw <hello@dkatsios.tk>
2021-04-26 19:07:33 +0300

    a

--- sw.js
+++ sw.js
@@ -20,3 +20,12 @@ self.addEventListener('install', async event => {
     const cache = await caches.open(cacheName); 
     await cache.addAll(staticAssets); 
   });
+  self.addEventListener('fetch', event => {
+    const req = event.request;
+    event.respondWith(cacheFirst(req));
+  });
+  async function cacheFirst(req) {
+    const cache = await caches.open(cacheName); 
+    const cachedResponse = await cache.match(req); 
+    return cachedResponse || fetch(req); 
+  }
