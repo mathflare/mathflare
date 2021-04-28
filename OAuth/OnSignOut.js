f9cb48d770dc614b5028155f7b84738bab3fd802
dk-raw <hello@dkatsios.tk>
2021-04-28 12:58:43 +0300

    minor bug fix

--- OAuth/OnSignOut.js
+++ OAuth/OnSignOut.js
@@ -2,5 +2,6 @@
     $("#signout").click(function (e) {
         e.preventDefault();
         $("#display").text("Not Signed In");
+        $("#user-image").attr("src", "Assets/profile.jpg"); 
     });
 });
