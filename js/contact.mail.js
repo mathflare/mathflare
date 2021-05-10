0de7895282aff112aa93840ac8deacc128af39f9
dk-raw <hello@dkatsios.tk>
2021-05-10 21:27:10 +0300

    imporved user messages in the contact page

--- js/contact.mail.js
+++ js/contact.mail.js
@@ -5,17 +5,32 @@
     const onSuccess = () => {
         console.log('email sent successfully')
         $("#contact-form").append(`
-            <div class="alert alert-success" role="alert">
-                <strong>Done!</strong> The email was sent successfully.
-            </div>
-                    `);
+            <p class="text-success user-message-mail" id="suc"><strong>Success!</strong> The email was sent successfully</p>
+        `);
         sendButton.val('Send');
         sendButton.prop('disabled',false);
         $("#subj").val(null);
         $("#msg").val(null);
+        setTimeout(function(){
+            $("#suc").remove();
+            }, 8000);
     }
     const onError = (error) => {
-        console.log('email was not sent successfully. ' + error)
+        console.error('email was not sent successfully. ' + error.error)
+        $("#contact-form").append(`
+                <p class="text-warning user-message-mail">
+                    the application encountered an error. 
+                    Please try again later or <u id="refresh">refresh the page</u>. If the problem presists, please <a href="https://github.com/liquid-geometry/liquidgeometry/issues" class="text-white"><u>report a bug</u></a>.
+                </p>
+        `);
+        sendButton.val('Error! Refresh the Page');
+        sendButton.prop('disabled',true);
+        $("#subj").val(null);
+        $("#msg").val(null);
+        $("#refresh").click(refresh = (e) => { 
+            e.preventDefault();
+            location.reload(); 
+        });
     }
     const sendButton = $("#" + form_id + " [name='send']");
     const send = () => {
