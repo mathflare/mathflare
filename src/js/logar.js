2a4f05bdd27125e5b4be00e86881d3e95405cd57
dk-raw <hello@dkatsios.tk>
2021-06-13 12:03:44 +0300

    added more log cases

--- src/js/logar.js
+++ src/js/logar.js
@@ -3,8 +3,18 @@ $(document).ready(() => {
         event.preventDefault();
         const number = $("#number").val();
         const base = $("#base").val();
-        if (number == "" || base == "" || number <= 0 || base <= 0) {
+        if (number == "" || base == "") {
             $("#output").html(`<p class="text-warning">Please enter a valid value</p>`);
+            $("#number").val(null);
+            $("#base").val(10);
+        }
+        else if (number <= 0) {
+            $("#output").html(`<p class="text-warning">The logarithm is an indeterminate</p>`);
+            $("#number").val(null);
+            $("#base").val(10);
+        }
+        else if (base == 1 || base <= 0) {
+            $("#output").html(`<p class="text-warning">The logarithm is an indeterminate; it's base cannot be negative, 0 or 1 (base > 0 and base &#8800; 1)</p>`);
             $("#number").val(null);
             $("#base").val(10);
         }
