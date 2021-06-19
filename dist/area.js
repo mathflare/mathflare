133dfc4dc9a37e7c03320a5ed9d2e508c53d3440
dk-raw <hello@dkatsios.tk>
2021-06-19 16:21:11 +0300

    typescript, typescript, typescript....

--- dist/area.js
+++ dist/area.js
@@ -1,105 +1,115 @@
-//copyright (c) 2021 MathFlare LLC. - All rights reserved.
-$(document).ready(() => {
-    $("#squarecalc").click((e) => { 
-        e.preventDefault();
-        const side = $("#squareside").val();
-        if (side == "") {
-            $("#squareout").html(`<p class="text-warning">Please enter a valid value</p>`);
-            $("#squareside").val(null);
-        } else if (side <= 0) {
-            $("#squareout").html(`<p class="text-warning">The side must be a positive number</p>`);
-            $("#squareside").val(null);
-        } else {
-            $("#squareout").html(`<p class="text-success">The area of the square is A = ${side * side}cm<sup>2</sup></p>`);
-            $("#squareside").val(null);
-        }
-    });
-    $("#paracalc").click((e) => { 
-        e.preventDefault();
-        const width = $("#paraw").val();
-        const height = $("#parah").val();
-        if (width == "" || height == "") {
-            $("#paraout").html(`<p class="text-warning">Please enter a valid value</p>`);
-            $("#paraw").val(null);
-            $("#parah").val(null);
-        } else if (width <= 0 || height <= 0) {
-            $("#paraout").html(`<p class="text-warning">Both the width and the height must be a positive number</p>`);
-            $("#paraw").val(null);
-            $("#parah").val(null);
-        } else {
-            $("#paraout").html(`<p class="text-success">The area of the parallelogram is A = ${width * height}cm<sup>2</sup></p>`);
-            $("#paraw").val(null);
-            $("#parah").val(null);
-        }
-    });
-    $("#tricalc").click((e) => { 
-        e.preventDefault();
-        const a = $("#tria").val();
-        const b = $("#trib").val();
-        if (a == "" || b == "") {
-            $("#triout").html(`<p class="text-warning">Please enter a valid value</p>`);
-            $("#tria").val(null);
-            $("#trib").val(null);
-        } else if (a <= 0 || b <= 0) {
-            $("#triout").html(`<p class="text-warning">Both the segment and the height must be a positive number</p>`);
-            $("#tria").val(null);
-            $("#trib").val(null);
-        } else {
-            $("#triout").html(`<p class="text-success">The area of the triangle is A = ${(a * b) /2}cm<sup>2</sup></p>`);
-            $("#tria").val(null);
-            $("#trib").val(null);
-        }
-    });
-    $("#etricalc").click((e) => { 
-        e.preventDefault();
-        const side = $("#etris").val();
-        if (side == "") {
-            $("#etriout").html(`<p class="text-warning">Please enter a valid value</p>`);
-            $("#etris").val(null);
-        } else if (side <= 0) {
-            $("#etriout").html(`<p class="text-warning">The side must be a positive number</p>`);
-            $("#etris").val(null);
-        } else {
-            $("#etriout").html(`<p class="text-success">The area of the equilateral triangle is A = ${(side * side * Math.sqrt(3)) / 4}cm<sup>2</sup> &#8730;3/4*${side}<sup>2</sup> = &#8730;3/4*${side * side}</p>`);
-            $("#etris").val(null);
-        }
-    });
-    $("#circlecalc").click((e) => { 
-        e.preventDefault();
-        const r = $("#circler").val();
-        const p = 3.14;
-        if (r == "") {
-            $("#circleout").html(`<p class="text-warning">Please enter a valid value</p>`);
-            $("#circler").val(null);
-        } else if (r <= 0) {
-            $("#circleout").html(`<p class="text-warning">The radius must be a positive number</p>`);
-            $("#circler").val(null);
-        } else {
-            $("#circleout").html(`<p class="text-success">The area of the circle is A &#8776; ${r * r * p}cm<sup>2</sup> (π = ${p})</p>`);
-            $("#circler").val(null);
-        }
-    });
-    $("#trapcalc").click((e) => { 
-        e.preventDefault();
-        const a = $("#trapa").val();
-        const b = $("#trapb").val();
-        const c = $("#trapc").val();
-        if (a == "" || b == "" || c == "") {
-            $("#trapout").html(`<p class="text-warning">Please enter a valid value</p>`);
-            $("#trapa").val(null);
-            $("#trapb").val(null);
-            $("#trapc").val(null);
-        } else if (a <= 0 || b <= 0) {
-            $("#trapout").html(`<p class="text-warning">Both the small/large segment and the height must be a positive number</p>`);
-            $("#trapa").val(null);
-            $("#trapb").val(null);
-            $("#trapc").val(null);
-        } else {
-            $("#trapout").html(`<p class="text-success">The area of the trapezoid is A = ${((parseFloat(a) + parseFloat(b)) * parseFloat(c)) / 2}cm<sup>2</sup></p>`);
-            $("#trapa").val(null);
-            $("#trapb").val(null);
-            $("#trapc").val(null);
-        }
-    });
+"use strict";
+document.getElementById('squarecalc').addEventListener('click', (event) => {
+    event.preventDefault();
+    const side = parseFloat(document.getElementById('squareside').value);
+    if (isNaN(side)) {
+        document.getElementById('squareout').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        document.getElementById('squareside').value = "";
+    }
+    else if (side <= 0) {
+        document.getElementById('squareout').innerHTML = `<p class="text-warning">The side must be a positive number</p>`;
+        document.getElementById('squareside').value = "";
+    }
+    else {
+        document.getElementById('squareout').innerHTML = `<p class="text-success">The area of the square is A = ${side * side}cm<sup>2</sup></p>`;
+        document.getElementById('squareside').value = "";
+    }
 });
-//copyright (c) 2021 MathFlare LLC. - All rights reserved.
+document.getElementById('paracalc').addEventListener('click', (event) => {
+    event.preventDefault();
+    const width = parseFloat(document.getElementById('paraw').value);
+    const height = parseFloat(document.getElementById('parah').value);
+    if (isNaN(width) || isNaN(height)) {
+        document.getElementById('paraout').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        document.getElementById('paraw').value = "";
+        document.getElementById('parah').value = "";
+    }
+    else if (width <= 0 || height <= 0) {
+        document.getElementById('paraout').innerHTML = `<p class="text-warning">Both the width and the height must be a positive number</p>`;
+        document.getElementById('paraw').value = "";
+        document.getElementById('parah').value = "";
+    }
+    else {
+        document.getElementById('paraout').innerHTML = `<p class="text-success">The area of the parallelogram is A = ${width * height}cm<sup>2</sup></p>`;
+        document.getElementById('paraw').value = "";
+        document.getElementById('parah').value = "";
+    }
+});
+document.getElementById('tricalc').addEventListener('click', (event) => {
+    event.preventDefault();
+    const a = parseFloat(document.getElementById('tria').value);
+    const b = parseFloat(document.getElementById('trib').value);
+    if (isNaN(a) || isNaN(b)) {
+        document.getElementById('triout').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        document.getElementById('tria').value = "";
+        document.getElementById('trib').value = "";
+    }
+    else if (a <= 0 || b <= 0) {
+        document.getElementById('triout').innerHTML = `<p class="text-warning">Both the segment and the height must be a positive number</p>`;
+        document.getElementById('tria').value = "";
+        document.getElementById('trib').value = "";
+    }
+    else {
+        document.getElementById('triout').innerHTML = `<p class="text-success">The area of the triangle is A = ${(a * b) / 2}cm<sup>2</sup></p>`;
+        document.getElementById('tria').value = "";
+        document.getElementById('trib').value = "";
+    }
+});
+document.getElementById('etricalc').addEventListener('click', (event) => {
+    event.preventDefault();
+    const side = parseFloat(document.getElementById('etris').value);
+    if (isNaN(side)) {
+        document.getElementById('etriout').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        document.getElementById('etris').value = "";
+    }
+    else if (side <= 0) {
+        document.getElementById('etriout').innerHTML = `<p class="text-warning">The side must be a positive number</p>`;
+        document.getElementById('etris').value = "";
+    }
+    else {
+        document.getElementById('etriout').innerHTML = `<p class="text-success">The area of the equilateral triangle is A = ${(side * side * Math.sqrt(3)) / 4}cm<sup>2</sup> (&#8764; ${((side * side * Math.sqrt(3)) / 4).toFixed(3)})</p>`;
+        document.getElementById('etris').value = "";
+    }
+});
+document.getElementById('circlecalc').addEventListener('click', (event) => {
+    event.preventDefault();
+    const r = parseFloat(document.getElementById('circler').value);
+    const p = 3.14;
+    const pperc = 3.14159265;
+    if (isNaN(r)) {
+        document.getElementById('circleout').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        document.getElementById('circler').value = "";
+    }
+    else if (r <= 0) {
+        document.getElementById('circleout').innerHTML = `<p class="text-warning">The radius must be a positive number</p>`;
+        document.getElementById('circler').value = "";
+    }
+    else {
+        document.getElementById('circleout').innerHTML = `<p class="text-success">The area of the circle is A &#8776; ${r * r * p}cm<sup>2</sup> (A &#8776; ${r * r * pperc})</p>`;
+        document.getElementById('circler').value = "";
+    }
+});
+document.getElementById('trapcalc').addEventListener('click', (event) => {
+    event.preventDefault();
+    const a = parseFloat(document.getElementById('trapa').value);
+    const b = parseFloat(document.getElementById('trapb').value);
+    const c = parseFloat(document.getElementById('trapc').value);
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        document.getElementById('trapout').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        document.getElementById('trapa').value = "";
+        document.getElementById('trapb').value = "";
+        document.getElementById('trapc').value = "";
+    }
+    else if (a <= 0 || b <= 0 || c <= 0) {
+        document.getElementById('trapout').innerHTML = `<p class="text-warning">Both the small/large segment and the height must be a positive number</p>`;
+        document.getElementById('trapa').value = "";
+        document.getElementById('trapb').value = "";
+        document.getElementById('trapc').value = "";
+    }
+    else {
+        document.getElementById('trapout').innerHTML = `<p class="text-success">The area of the trapezoid is A = ${((a + b) * c) / 2}cm<sup>2</sup></p>`;
+        document.getElementById('trapa').value = "";
+        document.getElementById('trapb').value = "";
+        document.getElementById('trapc').value = "";
+    }
+});
---
+++ src/area.ts
@@ -0,0 +1,124 @@
+// square
+(document.getElementById('squarecalc')as HTMLButtonElement).addEventListener('click', (event) => {
+    event.preventDefault();
+    const side: number = parseFloat((< HTMLInputElement > document.getElementById('squareside')).value)!;
+    if (isNaN(side)) {
+        (document.getElementById('squareout')as HTMLElement).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        (< HTMLInputElement > document.getElementById('squareside')).value = "";
+    } else if (side <= 0) {
+        (document.getElementById('squareout')as HTMLElement).innerHTML = `<p class="text-warning">The side must be a positive number</p>`;
+        (< HTMLInputElement > document.getElementById('squareside')).value = "";
+    } else {
+        (document.getElementById('squareout')as HTMLElement).innerHTML = `<p class="text-success">The area of the square is A = ${
+            side * side
+        }cm<sup>2</sup></p>`;
+        (< HTMLInputElement > document.getElementById('squareside')).value = "";
+    }
+});
+// parallelogram
+(document.getElementById('paracalc')as HTMLButtonElement).addEventListener('click', (event) => {
+    event.preventDefault();
+    const width: number = parseFloat((< HTMLInputElement > document.getElementById('paraw')).value)!;
+    const height: number = parseFloat((< HTMLInputElement > document.getElementById('parah')).value)!;
+    if (isNaN(width) || isNaN(height)) {
+        (document.getElementById('paraout')as HTMLElement).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        (< HTMLInputElement > document.getElementById('paraw')).value = "";
+        (< HTMLInputElement > document.getElementById('parah')).value = "";
+    } else if (width <= 0 || height <= 0) {
+        (document.getElementById('paraout')as HTMLElement).innerHTML = `<p class="text-warning">Both the width and the height must be a positive number</p>`;
+        (< HTMLInputElement > document.getElementById('paraw')).value = "";
+        (< HTMLInputElement > document.getElementById('parah')).value = "";
+    } else {
+        (document.getElementById('paraout')as HTMLElement).innerHTML = `<p class="text-success">The area of the parallelogram is A = ${
+            width * height
+        }cm<sup>2</sup></p>`;
+        (< HTMLInputElement > document.getElementById('paraw')).value = "";
+        (< HTMLInputElement > document.getElementById('parah')).value = "";
+    }
+});
+// triangle
+(document.getElementById('tricalc')as HTMLButtonElement).addEventListener('click', (event) => {
+    event.preventDefault();
+    const a: number = parseFloat((< HTMLInputElement > document.getElementById('tria')).value)!;
+    const b: number = parseFloat((< HTMLInputElement > document.getElementById('trib')).value)!;
+    if (isNaN(a) || isNaN(b)) {
+        (document.getElementById('triout')as HTMLElement).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        (< HTMLInputElement > document.getElementById('tria')).value = "";
+        (< HTMLInputElement > document.getElementById('trib')).value = "";
+    } else if (a <= 0 || b <= 0) {
+        (document.getElementById('triout')as HTMLElement).innerHTML = `<p class="text-warning">Both the segment and the height must be a positive number</p>`;
+        (< HTMLInputElement > document.getElementById('tria')).value = "";
+        (< HTMLInputElement > document.getElementById('trib')).value = "";
+    } else {
+        (document.getElementById('triout')as HTMLElement).innerHTML = `<p class="text-success">The area of the triangle is A = ${
+            (a * b) / 2
+        }cm<sup>2</sup></p>`;
+        (< HTMLInputElement > document.getElementById('tria')).value = "";
+        (< HTMLInputElement > document.getElementById('trib')).value = "";
+    }
+});
+// equilateral triangle
+(document.getElementById('etricalc')as HTMLButtonElement).addEventListener('click', (event) => {
+    event.preventDefault();
+    const side: number = parseFloat((< HTMLInputElement > document.getElementById('etris')).value)!;
+    if (isNaN(side)) {
+        (document.getElementById('etriout')as HTMLElement).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        (< HTMLInputElement > document.getElementById('etris')).value = "";
+    } else if (side <= 0) {
+        (document.getElementById('etriout')as HTMLElement).innerHTML = `<p class="text-warning">The side must be a positive number</p>`;
+        (< HTMLInputElement > document.getElementById('etris')).value = "";
+    } else {
+        (document.getElementById('etriout')as HTMLElement).innerHTML = `<p class="text-success">The area of the equilateral triangle is A = ${
+            (side * side * Math.sqrt(3)) / 4
+        }cm<sup>2</sup> (&#8764; ${
+            ((side * side * Math.sqrt(3)) / 4).toFixed(3)
+        })</p>`;
+        (< HTMLInputElement > document.getElementById('etris')).value = "";
+    }
+});
+// circle
+(document.getElementById('circlecalc')as HTMLButtonElement).addEventListener('click', (event) => {
+    event.preventDefault();
+    const r: number = parseFloat((< HTMLInputElement > document.getElementById('circler')).value)!;
+    const p: number = 3.14 !;
+    const pperc: number = 3.14159265 !;
+    if (isNaN(r)) {
+        (document.getElementById('circleout')as HTMLElement).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        (< HTMLInputElement > document.getElementById('circler')).value = "";
+    } else if (r <= 0) {
+        (document.getElementById('circleout')as HTMLElement).innerHTML = `<p class="text-warning">The radius must be a positive number</p>`;
+        (< HTMLInputElement > document.getElementById('circler')).value = "";
+    } else {
+        (document.getElementById('circleout')as HTMLElement).innerHTML = `<p class="text-success">The area of the circle is A &#8776; ${
+            r * r * p
+        }cm<sup>2</sup> (A &#8776; ${
+            r * r * pperc
+        })</p>`;
+        (< HTMLInputElement > document.getElementById('circler')).value = "";
+    }
+});
+// trapezoid
+(document.getElementById('trapcalc')as HTMLButtonElement).addEventListener('click', (event) => {
+    event.preventDefault();
+    const a: number = parseFloat((< HTMLInputElement > document.getElementById('trapa')).value)!;
+    const b: number = parseFloat((< HTMLInputElement > document.getElementById('trapb')).value)!;
+    const c: number = parseFloat((< HTMLInputElement > document.getElementById('trapc')).value)!;
+    if (isNaN(a) || isNaN(b) || isNaN(c)) {
+        (document.getElementById('trapout')as HTMLElement).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        (< HTMLInputElement > document.getElementById('trapa')).value = "";
+        (< HTMLInputElement > document.getElementById('trapb')).value = "";
+        (< HTMLInputElement > document.getElementById('trapc')).value = "";
+    } else if (a <= 0 || b <= 0 || c <= 0) {
+        (document.getElementById('trapout')as HTMLElement).innerHTML = `<p class="text-warning">Both the small/large segment and the height must be a positive number</p>`;
+        (< HTMLInputElement > document.getElementById('trapa')).value = "";
+        (< HTMLInputElement > document.getElementById('trapb')).value = "";
+        (< HTMLInputElement > document.getElementById('trapc')).value = "";
+    } else {
+        (document.getElementById('trapout')as HTMLElement).innerHTML = `<p class="text-success">The area of the trapezoid is A = ${
+            ((a + b) * c) / 2
+        }cm<sup>2</sup></p>`;
+        (< HTMLInputElement > document.getElementById('trapa')).value = "";
+        (< HTMLInputElement > document.getElementById('trapb')).value = "";
+        (< HTMLInputElement > document.getElementById('trapc')).value = "";
+    }
+});
