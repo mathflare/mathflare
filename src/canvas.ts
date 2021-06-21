d8d52342a9e4c01bbef9438079b9d3f838cf304c
dk-raw <hello@dkatsios.tk>
2021-06-21 18:34:36 +0300

    expirementing with graphs

---
+++ src/canvas.ts
@@ -0,0 +1,38 @@
+const canvas: any = <HTMLCanvasElement> document.getElementById('canvas')!;
+const ctx: any = canvas.getContext("2d")!;
+ctx.translate(canvas.width/2,canvas.height/2);
+ctx.strokeStyle = '#FFFFFF';
+
+//Vertical Referance Line
+ctx.beginPath(); 
+ctx.strokeStyle = '#DC3545';
+ctx.lineWidth = 5;
+ctx.moveTo(0, 300);
+ctx.lineTo(0, -300);
+ctx.stroke();
+
+//Vertical Referance Line
+ctx.beginPath(); 
+ctx.strokeStyle = '#DC3545';
+ctx.lineWidth = 5;
+ctx.moveTo(300, 0);
+ctx.lineTo(-300, 0);
+ctx.stroke();
+
+//First User Line
+ctx.beginPath();   
+ctx.strokeStyle = '#FFC107';
+ctx.lineWidth = 7;
+ctx.lineCap = "round";
+ctx.moveTo(0, 0);
+ctx.lineTo(245, -365);
+ctx.stroke();
+
+//Second User Line
+ctx.beginPath();   
+ctx.strokeStyle = '#0D6EFD';
+ctx.lineWidth = 7;
+ctx.lineCap = "round";
+ctx.moveTo(0, 0);
+ctx.lineTo(76, 256);
+ctx.stroke();
