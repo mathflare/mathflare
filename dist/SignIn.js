3bfdcaa753df2ae3e6c0c70f281998cd5f8b3977
dk-raw <hello@dkatsios.tk>
2021-06-23 15:51:36 +0300

    fixed google OAuth

--- dist/SignIn.js
+++ dist/SignIn.js
@@ -3,16 +3,6 @@
   var current = new Date();
   var PhotoUrl = profile.getImageUrl();
   var UserAgent = navigator.userAgent;
-  const Http = new XMLHttpRequest();
-  const url = "https://ipgeolocation.abstractapi.com/v1/?api_key=e3bc23053c584ae2bbf332d89bc66f8d";
-  Http.open("GET", url);
-  Http.send();
-  Http.onreadystatechange = () => {
-    const response = Http.responseText;
-    console.log("ISP: " + response.connection.isp_name);
-    console.log("City: " + response.city + "," + response.country)
-    console.log("Is VPN: " + response.security.is_vpn);
-  };
   $("#display").text("Signed In As " + profile.getName());
   console.log("=================================================");
   console.log("User signed in at " + current);
