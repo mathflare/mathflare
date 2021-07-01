0ed6bf68504b443700e24ba7477a808320d88a91
dk-raw <hello@dkatsios.tk>
2021-07-01 15:10:44 +0300

    minor changes

--- dist/copy.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(() => {
-    $("#copy").click((e) => { 
-        e.preventDefault();
-        $("#copy").removeClass("bi-clipboard");
-        $("#copy").addClass("bi-clipboard-check");
-    });
-});
