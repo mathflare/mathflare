2346de37c79a301607a14bcf4abb6dfa3fe0c3b8
dk-raw <hello@dkatsios.tk>
2021-07-02 15:16:45 +0300

    imporved factorial performance

--- dist/fact.js
+++ dist/fact.js
@@ -10,18 +10,16 @@ document.getElementById('button').addEventListener('click', (event) => {
         document.getElementById('output').innerHTML = `<p class="text-warning">The number must be a natural number (>0 and not decimal)</p>`;
         document.getElementById('number').value = "";
     }
+    else if (num >= 170) {
+        document.getElementById('output').innerHTML = `<p class="text-warning">The number is too large!</p>`;
+        document.getElementById('number').value = "";
+    }
     else {
         let f = 1;
         for (let i = 1; i <= num; i++) {
             f = f * i;
         }
-        if (f === Number.POSITIVE_INFINITY) {
-            document.getElementById('output').innerHTML = `<p class="text-warning">The number is too large!</p>`;
-            document.getElementById('number').value = "";
-        }
-        else {
-            document.getElementById('output').innerHTML = `<p class="text-success">${num}! = ${f}</p>`;
-            document.getElementById('number').value = "";
-        }
+        document.getElementById('output').innerHTML = `<p class="text-success">${num}! = ${f}</p>`;
+        document.getElementById('number').value = "";
     }
 });
--- src/fact.ts
+++ src/fact.ts
@@ -1,3 +1,4 @@
+//copyright (c) 2021 MathFlare LLC.
 (<HTMLButtonElement>document.getElementById('button')).addEventListener('click', (event) => {
     event.preventDefault();
     const num: number = parseFloat((<HTMLInputElement>document.getElementById('number')).value)!;
@@ -7,17 +8,15 @@
     } else if (num < 0 || !(Math.floor(num) == num)) {
         (document.getElementById('output') as HTMLElement).innerHTML = `<p class="text-warning">The number must be a natural number (>0 and not decimal)</p>`;
         (<HTMLInputElement>document.getElementById('number')).value = "";
+    } else if (num >= 170) { //above 170 is infinity anyways so why waste resources in calculating it.
+        (document.getElementById('output') as HTMLElement).innerHTML = `<p class="text-warning">The number is too large!</p>`;
+        (<HTMLInputElement>document.getElementById('number')).value = "";
     } else {
         let f: number = 1;
         for (let i = 1; i <= num; i++) {
             f = f * i;
         }
-        if (f === Number.POSITIVE_INFINITY) {
-            (document.getElementById('output') as HTMLElement).innerHTML = `<p class="text-warning">The number is too large!</p>`;
-            (<HTMLInputElement>document.getElementById('number')).value = "";
-        } else {
-            (document.getElementById('output') as HTMLElement).innerHTML = `<p class="text-success">${num}! = ${f}</p>`;
-            (<HTMLInputElement>document.getElementById('number')).value = "";
-        }
+        (document.getElementById('output') as HTMLElement).innerHTML = `<p class="text-success">${num}! = ${f}</p>`;
+        (<HTMLInputElement>document.getElementById('number')).value = "";
     }
 });
