55ade2a2c5f5fdd0c618b5a53e016adf8fefb152
dk-raw <hello@dkatsios.tk>
2021-07-05 11:24:51 +0300

    prod: use typescript

--- src/stats.ts
+++ src/stats.ts
@@ -1,5 +1,4 @@
-"use strict";
-var storage = window.localStorage;
+var storage: any = window.localStorage;
 window.addEventListener('load', () => {
     loadStats();
 });
@@ -8,19 +7,18 @@ const loadStats = () => {
     const equations = storage.getItem("equation");
     const formulas = storage.getItem("formulas");
     const ineq = storage.getItem("ineq");
-    document.getElementById('page-views').innerText = pageviews;
-    document.getElementById('equations-solved').innerText = equations;
-    document.getElementById('formulas-views').innerText = formulas;
-    document.getElementById('ineq').innerText = ineq;
+    (<HTMLParagraphElement> document.getElementById('page-views')).innerText = pageviews !;
+    (<HTMLParagraphElement> document.getElementById('equations-solved')).innerText = equations !;
+    (<HTMLParagraphElement> document.getElementById('formulas-views')).innerText = formulas !;
+    (<HTMLParagraphElement> document.getElementById('ineq')).innerText = ineq !;
     if (!storage.getItem("statCount")) {
         storage.setItem("statCount", 1);
-    }
-    else {
-        let statCount = storage.getItem("statCount");
+    } else {
+        let statCount: any = storage.getItem("statCount");
         statCount = parseFloat(statCount) + 1;
         storage.setItem("statCount", statCount);
     }
-};
+}
 const clearStats = () => {
     storage.clear();
     reload();
