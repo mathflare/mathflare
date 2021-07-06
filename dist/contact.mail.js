2a46123b2db5698c1b536d3c41cefeed0f0eef4e
dk-raw <hello@dkatsios.tk>
2021-07-06 11:12:44 +0300

    refactor: minor changes

--- dist/contact.mail.js
+++ dist/contact.mail.js
@@ -28,7 +28,7 @@ const js_send = () => {
     var message = document.querySelector("#" + form_id_js + " [name='text']").value;
     data_js['subject'] = subject;
     data_js['text'] = message;
-    var params = toParams(data_js);
+    let params = toParams(data_js);
     request.open("POST", "https://postmail.invotes.com/send", true);
     request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     request.send(params);
@@ -36,13 +36,13 @@ const js_send = () => {
 };
 sendButton.onclick = js_send;
 const toParams = (data_js) => {
-    var form_data = [];
-    for (var key in data_js) {
+    let form_data = [];
+    for (let key in data_js) {
         form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
     }
     return form_data.join("&");
 };
-var js_form = document.getElementById(form_id_js);
+const js_form = document.getElementById(form_id_js);
 js_form.addEventListener("submit", function (e) {
     e.preventDefault();
 });
