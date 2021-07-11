d42b3eef8f2e68a5e7e308f74acb2f7dc65a84c8
dk-raw <hello@dkatsios.tk>
2021-07-11 14:48:53 +0300

    refactor: minor changes

---
+++ dist/back-to-top.js
@@ -0,0 +1,12 @@
+"use strict";
+const backToTopBtn = document.getElementById('btn-back-to-top');
+window.onscroll = () => {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50)
+        backToTopBtn.style.display = 'block';
+    else
+        backToTopBtn.style.display = 'none';
+};
+backToTopBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
