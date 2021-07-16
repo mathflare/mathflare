7a3e131324bbc735402a60f7c574b6a017aa7449
dk-raw <hello@dkatsios.tk>
2021-07-16 14:04:11 +0300

    prod: added shared navbar

--- dist/navbar.js
+++ dist/navbar.js
@@ -1,1 +1,9 @@
-"use strict";
+$("#nav-container").load("../public/components/navbar.html", () => {
+  let aList = document.querySelectorAll("a");
+  for (let i = 0; i < aList.length; i++) {
+    let a = aList[i];
+    if (a.href.indexOf(window.location.pathname.replace('.html', '')) !== -1) {
+      a.classList.add("aactive");
+    }
+  }
+});
