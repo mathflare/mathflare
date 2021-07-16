5418ee7b23805b355d99535b360a3441081a4505
dk-raw <hello@dkatsios.tk>
2021-07-16 14:27:59 +0300

    fix: accounted for index

--- dist/navbar.js
+++ dist/navbar.js
@@ -2,8 +2,12 @@ $("#nav-container").load("../public/components/navbar.html", () => {
   let aList = document.querySelectorAll("a");
   for (let i = 0; i < aList.length; i++) {
     let a = aList[i];
-    if (a.href.indexOf(window.location.pathname.replace('.html', '')) !== -1) {
-      a.classList.add("aactive");
+    if (window.location.pathname.replace(".html", "") != "/") {
+      if (
+        a.href.indexOf(window.location.pathname.replace(".html", "")) !== -1
+      ) {
+        a.classList.add("aactive");
+      }
     }
   }
 });
