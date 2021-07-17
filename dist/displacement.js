addf33f394c83f88ed893e9ff10a8854c3067d2e
dk-raw <hello@dkatsios.tk>
2021-07-17 10:46:25 +0300

    prod: added displacement calculator

--- dist/displacement.js
+++ dist/displacement.js
@@ -1,26 +1,76 @@
 "use strict";
-document.getElementById('dxcalc').addEventListener('click', (event) => {
+document.getElementById('dxcalc1').addEventListener('click', (event) => {
     event.preventDefault();
-    const u = parseFloat(document.getElementById('u').value);
-    const dt = parseFloat(document.getElementById('dt').value);
-    if (isNaN(u) || isNaN(dt)) {
-        document.getElementById('dxout').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
-        document.getElementById('u').value = "";
-        document.getElementById('dt').value = "";
+    const v = parseFloat(document.getElementById('v1').value);
+    const dt = parseFloat(document.getElementById('dt1').value);
+    const clearValues = () => {
+        document.getElementById('v1').value = '';
+        document.getElementById('dt1').value = '';
+    };
+    if (isNaN(v) || isNaN(dt)) {
+        document.getElementById('dxout1').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        clearValues();
     }
-    else if (u <= 0) {
-        document.getElementById('dxout').innerHTML = `<p class="text-warning">The speed must be a positive number</p>`;
-        document.getElementById('u').value = "";
-        document.getElementById('dt').value = "";
+    else if (v <= 0) {
+        document.getElementById('dxout1').innerHTML = `<p class="text-warning">The speed must be a positive number</p>`;
+        clearValues();
     }
     else if (dt <= 0) {
-        document.getElementById('dxout').innerHTML = `<p class="text-warning">Δt must be a positive number</p>`;
-        document.getElementById('u').value = "";
-        document.getElementById('dt').value = "";
+        document.getElementById('dxout1').innerHTML = `<p class="text-warning">The Δt must be a positive number</p>`;
+        clearValues();
     }
     else {
-        document.getElementById('dxout').innerHTML = `<p class="text-success">Δx = ${u * dt}</p>`;
-        document.getElementById('u').value = "";
-        document.getElementById('dt').value = "";
+        document.getElementById('dxout1').innerHTML = `<p class="text-success">Δx = ${v * dt}</p>`;
+        clearValues();
+    }
+});
+document.getElementById('dtcalc2').addEventListener('click', (event) => {
+    event.preventDefault();
+    const v = parseFloat(document.getElementById('v2').value);
+    const dx = parseFloat(document.getElementById('dx2').value);
+    const clearValues = () => {
+        document.getElementById('v2').value = '';
+        document.getElementById('dx2').value = '';
+    };
+    if (isNaN(v) || isNaN(dx)) {
+        document.getElementById('dtout2').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        clearValues();
+    }
+    else if (v <= 0) {
+        document.getElementById('dtout2').innerHTML = `<p class="text-warning">The speed must be a positive number</p>`;
+        clearValues();
+    }
+    else if (dx <= 0) {
+        document.getElementById('dtout2').innerHTML = `<p class="text-warning">The Δx must be a positive number</p>`;
+        clearValues();
+    }
+    else {
+        document.getElementById('dtout2').innerHTML = `<p class="text-success">Δt = ${dx / v}</p>`;
+        clearValues();
+    }
+});
+document.getElementById('vcalc3').addEventListener('click', (event) => {
+    event.preventDefault();
+    const dx = parseFloat(document.getElementById('dx3').value);
+    const dt = parseFloat(document.getElementById('dt3').value);
+    const clearValues = () => {
+        document.getElementById('dx3').value = '';
+        document.getElementById('dt3').value = '';
+    };
+    if (isNaN(dt) || isNaN(dx)) {
+        document.getElementById('vout3').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        clearValues();
+    }
+    else if (dt <= 0) {
+        document.getElementById('vout3').innerHTML = `<p class="text-warning">The Δt must be a positive number</p>`;
+        clearValues();
+    }
+    else if (dx <= 0) {
+        document.getElementById('vout3').innerHTML = `<p class="text-warning">The Δx must be a positive number</p>`;
+        clearValues();
+    }
+    else {
+        document.getElementById('vout3').innerHTML = `<p class="text-success">v = ${dx / dt}</p>`;
+        clearValues();
     }
 });
--- src/displacement.ts
+++ src/displacement.ts
@@ -1,22 +1,69 @@
-(<HTMLButtonElement>document.getElementById('dxcalc')).addEventListener('click', (event) => {
+//Δx
+(<HTMLButtonElement>document.getElementById('dxcalc1')).addEventListener('click', (event) => {
     event.preventDefault();
-    const u: number = parseFloat((<HTMLInputElement>document.getElementById('u')).value)!;
-    const dt: number = parseFloat((<HTMLInputElement>document.getElementById('dt')).value)!;
-    if (isNaN(u) || isNaN(dt)) {
-        (<HTMLParagraphElement>document.getElementById('dxout')).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
-        (<HTMLInputElement>document.getElementById('u')).value = "";
-        (<HTMLInputElement>document.getElementById('dt')).value = "";
-    } else if (u <= 0) {
-        (<HTMLParagraphElement>document.getElementById('dxout')).innerHTML = `<p class="text-warning">The speed must be a positive number</p>`;
-        (<HTMLInputElement>document.getElementById('u')).value = "";
-        (<HTMLInputElement>document.getElementById('dt')).value = "";
+    const v: number = parseFloat((<HTMLInputElement>document.getElementById('v1')).value)!;
+    const dt: number = parseFloat((<HTMLInputElement>document.getElementById('dt1')).value)!;
+    const clearValues = () => {
+        (<HTMLInputElement>document.getElementById('v1')).value = '';
+        (<HTMLInputElement>document.getElementById('dt1')).value = '';
+    }
+    if (isNaN(v) || isNaN(dt)) {
+        (<HTMLParagraphElement>document.getElementById('dxout1')).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        clearValues();
+    } else if (v <= 0) {
+        (<HTMLParagraphElement>document.getElementById('dxout1')).innerHTML = `<p class="text-warning">The speed must be a positive number</p>`;
+        clearValues();
     } else if (dt <= 0) {
-        (<HTMLParagraphElement>document.getElementById('dxout')).innerHTML = `<p class="text-warning">Δt must be a positive number</p>`;
-        (<HTMLInputElement>document.getElementById('u')).value = "";
-        (<HTMLInputElement>document.getElementById('dt')).value = "";
+        (<HTMLParagraphElement>document.getElementById('dxout1')).innerHTML = `<p class="text-warning">The Δt must be a positive number</p>`;
+        clearValues();
     } else {
-        (<HTMLParagraphElement>document.getElementById('dxout')).innerHTML = `<p class="text-success">Δx = ${u * dt}</p>`;
-        (<HTMLInputElement>document.getElementById('u')).value = "";
-        (<HTMLInputElement>document.getElementById('dt')).value = "";
+        (<HTMLParagraphElement>document.getElementById('dxout1')).innerHTML = `<p class="text-success">Δx = ${v * dt}</p>`;
+        clearValues();
+    }
+});
+//Δt
+(<HTMLButtonElement>document.getElementById('dtcalc2')).addEventListener('click', (event) => {
+    event.preventDefault();
+    const v: number = parseFloat((<HTMLInputElement>document.getElementById('v2')).value)!;
+    const dx: number = parseFloat((<HTMLInputElement>document.getElementById('dx2')).value)!;
+    const clearValues = () => {
+        (<HTMLInputElement>document.getElementById('v2')).value = '';
+        (<HTMLInputElement>document.getElementById('dx2')).value = '';
+    }
+    if (isNaN(v) || isNaN(dx)) {
+        (<HTMLParagraphElement>document.getElementById('dtout2')).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        clearValues();
+    } else if (v <= 0) {
+        (<HTMLParagraphElement>document.getElementById('dtout2')).innerHTML = `<p class="text-warning">The speed must be a positive number</p>`;
+        clearValues();
+    } else if (dx <= 0) {
+        (<HTMLParagraphElement>document.getElementById('dtout2')).innerHTML = `<p class="text-warning">The Δx must be a positive number</p>`;
+        clearValues();
+    } else {
+        (<HTMLParagraphElement>document.getElementById('dtout2')).innerHTML = `<p class="text-success">Δt = ${dx / v}</p>`;
+        clearValues();
+    }
+});
+//v
+(<HTMLButtonElement>document.getElementById('vcalc3')).addEventListener('click', (event) => {
+    event.preventDefault();
+    const dx: number = parseFloat((<HTMLInputElement>document.getElementById('dx3')).value)!;
+    const dt: number = parseFloat((<HTMLInputElement>document.getElementById('dt3')).value)!;
+    const clearValues = () => {
+        (<HTMLInputElement>document.getElementById('dx3')).value = '';
+        (<HTMLInputElement>document.getElementById('dt3')).value = '';
+    }
+    if (isNaN(dt) || isNaN(dx)) {
+        (<HTMLParagraphElement>document.getElementById('vout3')).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        clearValues();
+    } else if (dt <= 0) {
+        (<HTMLParagraphElement>document.getElementById('vout3')).innerHTML = `<p class="text-warning">The Δt must be a positive number</p>`;
+        clearValues();
+    } else if (dx <= 0) {
+        (<HTMLParagraphElement>document.getElementById('vout3')).innerHTML = `<p class="text-warning">The Δx must be a positive number</p>`;
+        clearValues();
+    } else {
+        (<HTMLParagraphElement>document.getElementById('vout3')).innerHTML = `<p class="text-success">v = ${dx / dt}</p>`;
+        clearValues();
     }
 });
