422b6da305c5dbed4a214445f3c265b6bd93c0f7
dk-raw <hello@dkatsios.tk>
2021-07-19 11:55:19 +0300

    fix: handshake calc

--- dist/handshake.js
+++ dist/handshake.js
@@ -6,6 +6,10 @@ document.getElementById('calc').addEventListener('click', (event) => {
         document.getElementById('output').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
         document.getElementById('n').value = '';
     }
+    else if (n < 0) {
+        document.getElementById('output').innerHTML = `<p class="text-warning">The number of visitors must be a positive number</p>`;
+        document.getElementById('n').value = '';
+    }
     else {
         document.getElementById('output').innerHTML = `<p class="text-success">There have been ${(Math.pow(n, 2) - n) / 2} handshakes at that party</p>`;
         document.getElementById('n').value = '';
--- src/handshake.ts
+++ src/handshake.ts
@@ -4,6 +4,9 @@
     if (isNaN(n) || Math.floor(n) !== n) {
         (<HTMLParagraphElement>document.getElementById('output')).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
         (<HTMLInputElement>document.getElementById('n')).value = '';
+    } else if (n < 0) {
+        (<HTMLParagraphElement>document.getElementById('output')).innerHTML = `<p class="text-warning">The number of visitors must be a positive number</p>`;
+        (<HTMLInputElement>document.getElementById('n')).value = '';
     } else {
         (<HTMLParagraphElement>document.getElementById('output')).innerHTML = `<p class="text-success">There have been ${(Math.pow(n, 2) - n) / 2} handshakes at that party</p>`;
         (<HTMLInputElement>document.getElementById('n')).value = '';
