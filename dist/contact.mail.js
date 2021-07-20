25f1ddc21bbe98b99479d394e905b3e43acc41d1
dk-raw <hello@dkatsios.tk>
2021-07-20 15:34:57 +0300

    prod: fixed contact form and added download tos option

--- dist/contact.mail.js
+++ dist/contact.mail.js
@@ -7,32 +7,45 @@ const js_onSuccess = () => {
     console.log('message sent successfully');
     sendButton.value = 'Send';
     sendButton.disabled = false;
-    document.getElementById('user-message').innerHTML = `<p class="text-success">Your message has been sent successfully. <br> We will get back to you soon.</p>`;
-    document.getElementById('contact-form').reset();
+    document.querySelector('#user-message').innerHTML = `<p class="text-success">Your message has been sent successfully. <br> We will get back to you soon.</p>`;
+    document.querySelector('#contact-form').reset();
 };
-const js_onError = (error) => { console.error(error); document.getElementById('user-message').innerHTML = `<p class="text-warning">The application encountered an error. Please <u onclick="refresh()" style="cursor: pointer;" class="text-white">refresh the page</u> and try again.</p>`; sendButton.value = 'Error!'; };
-const sendButton = document.getElementById('send');
+const js_onError = (error) => {
+    console.error(error);
+    document.querySelector('#user-message').innerHTML = `<p class="text-warning">The application encountered an error. Please <u onclick="refresh()" style="cursor: pointer;" class="text-white">refresh the page</u> and try again.</p>`;
+    sendButton.value = 'Error!';
+};
+const sendButton = document.querySelector('#send');
 const js_send = () => {
-    sendButton.value = 'Sending…';
-    sendButton.disabled = true;
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            js_onSuccess();
-        }
-        else if (request.readyState == 4) {
-            js_onError(request.response);
-        }
-    };
-    var subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
-    var message = document.querySelector("#" + form_id_js + " [name='text']").value;
-    data_js['subject'] = subject;
-    data_js['text'] = message;
-    let params = toParams(data_js);
-    request.open("POST", "https://postmail.invotes.com/send", true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    request.send(params);
-    return false;
+    if (document.querySelector("#" + form_id_js + " [name='email']").value === ''
+        || document.querySelector("#" + form_id_js + " [name='subject']").value === ''
+        || document.querySelector("#" + form_id_js + " [name='text']").value === ''
+        || !document.querySelector('#agree-to-tos').checked
+        || document.querySelector('.h-captcha').children[0].getAttribute('data-hcaptcha-response') === '') {
+        document.querySelector('#user-message').innerHTML = `<p class="text-warning">Please fill out all the fields</p>`;
+    }
+    else {
+        sendButton.value = 'Sending…';
+        sendButton.disabled = true;
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                js_onSuccess();
+            }
+            else if (request.readyState == 4) {
+                js_onError(request.response);
+            }
+        };
+        let subject = document.querySelector("#" + form_id_js + " [name='email']").value + " | " + document.querySelector("#" + form_id_js + " [name='subject']").value;
+        let message = document.querySelector("#" + form_id_js + " [name='text']").value;
+        data_js['subject'] = subject;
+        data_js['text'] = message;
+        let params = toParams(data_js);
+        request.open("POST", "https://postmail.invotes.com/send", true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send(params);
+        return false;
+    }
 };
 sendButton.onclick = js_send;
 const toParams = (data_js) => {
@@ -42,9 +55,9 @@ const toParams = (data_js) => {
     }
     return form_data.join("&");
 };
-const js_form = document.getElementById(form_id_js);
-js_form.addEventListener("submit", function (e) {
-    e.preventDefault();
+const js_form = document.querySelector("#" + form_id_js);
+js_form.addEventListener("submit", (event) => {
+    event.preventDefault();
 });
 const refresh = () => {
     location.reload();
--- src/contact.mail.ts
+++ src/contact.mail.ts
@@ -6,32 +6,44 @@ const js_onSuccess = () => {
     console.log('message sent successfully');
     sendButton.value = 'Send';
     sendButton.disabled = false;
-    (<HTMLParagraphElement>document.getElementById('user-message')).innerHTML = `<p class="text-success">Your message has been sent successfully. <br> We will get back to you soon.</p>`;
-    (<HTMLFormElement>document.getElementById('contact-form')).reset()!;
+    (<HTMLParagraphElement>document.querySelector('#user-message')).innerHTML = `<p class="text-success">Your message has been sent successfully. <br> We will get back to you soon.</p>`;
+    (<HTMLFormElement>document.querySelector('#contact-form')).reset()!;
 }
-const js_onError = (error: object) => { console.error(error); (<HTMLParagraphElement>document.getElementById('user-message')).innerHTML = `<p class="text-warning">The application encountered an error. Please <u onclick="refresh()" style="cursor: pointer;" class="text-white">refresh the page</u> and try again.</p>`; sendButton.value = 'Error!'; }
-const sendButton: any = (<HTMLButtonElement>document.getElementById('send'))!;
+const js_onError = (error: object) => {
+    console.error(error);
+    (<HTMLParagraphElement>document.querySelector('#user-message')).innerHTML = `<p class="text-warning">The application encountered an error. Please <u onclick="refresh()" style="cursor: pointer;" class="text-white">refresh the page</u> and try again.</p>`;
+    sendButton.value = 'Error!';
+}
+const sendButton: any = (<HTMLButtonElement>document.querySelector('#send'))!;
 const js_send = () => {
-    sendButton.value = 'Sending…';
-    sendButton.disabled = true;
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            js_onSuccess();
-        } else
-            if (request.readyState == 4) {
-                js_onError(request.response);
-            }
-    };
-    var subject = (<HTMLInputElement>document.querySelector("#" + form_id_js + " [name='subject']")).value!;
-    var message = (<HTMLTextAreaElement>document.querySelector("#" + form_id_js + " [name='text']")).value!;
-    data_js['subject'] = subject;
-    data_js['text'] = message;
-    let params = toParams(data_js);
-    request.open("POST", "https://postmail.invotes.com/send", true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    request.send(params);
-    return false;
+    if ((<HTMLInputElement>document.querySelector("#" + form_id_js + " [name='email']")).value === ''
+        || (<HTMLInputElement>document.querySelector("#" + form_id_js + " [name='subject']")).value === ''
+        || (<HTMLInputElement>document.querySelector("#" + form_id_js + " [name='text']")).value === ''
+        || !(<HTMLInputElement>document.querySelector('#agree-to-tos')).checked
+        || (<HTMLDivElement>document.querySelector('.h-captcha')).children[0].getAttribute('data-hcaptcha-response')) {
+        (<HTMLParagraphElement>document.querySelector('#user-message')).innerHTML = `<p class="text-warning">Please fill out all the fields</p>`;
+    } else {
+        sendButton.value = 'Sending…';
+        sendButton.disabled = true;
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                js_onSuccess();
+            } else
+                if (request.readyState == 4) {
+                    js_onError(request.response);
+                }
+        };
+        let subject: any = (<HTMLInputElement>document.querySelector("#" + form_id_js + " [name='email']")).value! + " | " + (<HTMLInputElement>document.querySelector("#" + form_id_js + " [name='subject']")).value!;
+        let message: any = (<HTMLTextAreaElement>document.querySelector("#" + form_id_js + " [name='text']")).value!;
+        data_js['subject'] = subject;
+        data_js['text'] = message;
+        let params = toParams(data_js);
+        request.open("POST", "https://postmail.invotes.com/send", true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send(params);
+        return false;
+    }
 }
 sendButton.onclick = js_send;
 const toParams = (data_js: any) => {
@@ -41,9 +53,9 @@ const toParams = (data_js: any) => {
     }
     return form_data.join("&");
 }
-const js_form: any = (<HTMLFormElement>document.getElementById(form_id_js))!;
-js_form.addEventListener("submit", function (e: any) {
-    e.preventDefault();
+const js_form: any = (<HTMLFormElement>document.querySelector("#" + form_id_js))!;
+js_form.addEventListener("submit", (event: any) => {
+    event.preventDefault();
 });
 const refresh = () => {
     location.reload();
