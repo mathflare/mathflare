557054396229e8fcd8c01a7620a40b1b64661230
dk-raw <hello@dkatsios.tk>
2021-07-29 11:48:23 +0300

    beta: swapped top navbar with sidebar

--- dist/navbar.js
+++ dist/navbar.js
@@ -1,13 +1,14 @@
 $("#nav-container").load("../public/components/navbar.html", () => {
-  let aList = document.querySelectorAll("a");
-  for (let i = 0; i < aList.length; i++) {
-    let a = aList[i];
-    if (window.location.pathname.replace(".html", "") != "/") {
-      if (
-        a.href.indexOf(window.location.pathname.replace(".html", "")) !== -1
-      ) {
-        a.classList.add("aactive");
-      }
+    let aList = document.querySelectorAll("a");
+    for (let i = 0; i < aList.length; i++) {
+        let a = aList[i];
+        if (window.location.pathname.replace(".html", "") != "/") {
+            if (
+                a.href.indexOf(window.location.pathname.replace(".html", "")) !== -1
+            ) {
+                a.classList.add("active");
+                break;
+            }
+        }
     }
-  }
 });
