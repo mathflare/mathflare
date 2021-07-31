3620d0c767ad1a45bef2a0f7770dc21dd481713b
dk-raw <hello@dkatsios.tk>
2021-07-31 09:59:02 +0300

    test: set up testing

--- dist/OnSignOut.js
+++
@@ -1,8 +0,0 @@
-﻿$(document).ready(function () {
-    $("#signout").click(function (e) {
-        e.preventDefault();
-        $("#display").text("Not Signed In");
-        //(not yet implimented)
-        //$("#profile-photo").html(`<i class="bi bi-person-circle"></i>`);
-    });
-});
--- dist/SignOut.js
+++
@@ -1,8 +0,0 @@
-﻿function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        var current = new Date();
-        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-        console.log('User signed out at ' + current);
-    });
-}
