189b7bba2a5b94afaea0439bac4586985ede18b3
dk-raw <ddkatsios@protonmail.com>
2021-08-10 22:27:07 +0300

    prod: added calculator search box

--- dist/navbar.js
+++ dist/navbar.js
@@ -3,9 +3,7 @@ $("#nav-container").load("../public/components/navbar.html", () => {
     for (let i = 0; i < aList.length; i++) {
         let a = aList[i];
         if (window.location.pathname.replace(".html", "") != "/") {
-            if (
-                a.href.indexOf(window.location.pathname.replace(".html", "")) !== -1
-            ) {
+            if (a.href.indexOf(window.location.pathname.replace(".html", "")) !== -1) {
                 a.classList.add("active");
                 break;
             }
