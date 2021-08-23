1b173567da9bb6fea694bfb89ee158882020ecd1
Sarmouts <saradis.m06@gmail.com>
2021-08-23 22:10:20 +0300

    prod: added a acid-metal reaction calculator

---
+++ dist/acid-metalreaction.js
@@ -0,0 +1,62 @@
+"use strict";
+const replaceOne = (str) => {
+    let final = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== '1') {
+            final += str[i];
+        }
+    }
+    ;
+    return final;
+};
+document.getElementById('acmetalcalc').addEventListener('click', (event) => {
+    event.preventDefault();
+    const x = parseFloat(document.getElementById('x').value);
+    const y = (document.getElementById('y').value);
+    const z = parseFloat(document.getElementById('z').value);
+    const m = (document.getElementById('m').value);
+    const v = parseFloat(document.getElementById('v').value);
+    const clearValues = () => {
+        document.getElementById('x').value = '';
+        document.getElementById('y').value = '';
+        document.getElementById('z').value = '';
+        document.getElementById('m').value = '';
+        document.getElementById('v').value = '';
+    };
+    if (isNaN(x) || y == '' || isNaN(z) || m == '' || isNaN(v)) {
+        document.getElementById('acmetalout').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        clearValues();
+    }
+    else if (x <= 0 || z <= 0 || v <= 0) {
+        document.getElementById('acmetalout').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        clearValues();
+    }
+    else if (Math.floor(x) !== x || Math.floor(z) !== z || Math.floor(v) !== v) {
+        document.getElementById('acmetalout').innerHTML = `<p class="text-warning">All values must be integers</p>`;
+        clearValues();
+    }
+    else if (x > 3 || v > 3) {
+        document.getElementById('acmetalout').innerHTML = `<p class="text-warning">The value of x or v cannot be more than 3</p>`;
+        clearValues();
+    }
+    else if (z > 1) {
+        document.getElementById('acmetalout').innerHTML = `<p class="text-warning">The value of z cannot be more than 1</p>`;
+        clearValues();
+    }
+    else if (m == "Cu" || m == "Ag" || m == "Au" || m == "Pt" || m == "cu" || m == "ag" || m == "au" || m == "pt") {
+        document.getElementById('acmetalout').innerHTML = `<p class="text-warning">There is not any reaction since the metal is one of these (Cu,Ag,Au,Pt)</p>`;
+        clearValues();
+    }
+    else {
+        if (x === v) {
+            const result = `<span class="text-warning">2</span>H<sub class="text-warning">${x}</sub><span class="text-warning">${y}</span><sub class="text-warning">${z}</sub> + <span class="text-warning">2</span><span class="text-warning">${m}</span> &#8594; <span class="text-warning">2</span><span class="text-warning">${m}</span><span class="text-warning">${y}</span> + <span class="text-warning">${x}</span>H<sub>2</sub>`;
+            document.getElementById('acmetalout').innerHTML = `${replaceOne(result)}`;
+            clearValues();
+        }
+        else {
+            const result = `<span class="text-warning">${2 * v}</span>H<sub class="text-warning">${x}</sub><span class="text-warning">${y}</span><sub class="text-warning">${z}</sub> + <span class="text-warning">${2 * x}</span><span class="text-warning">${m}</span> &#8594; <span class="text-warning">2</span><span class="text-warning">${m}</span><sub class="text-warning">${x}</sub><span class="text-warning">${y}</span><sub class="text-warning">${v}</sub> + <span class="text-warning">${x * v}</span>H<sub>2</sub>`;
+            document.getElementById('acmetalout').innerHTML = `${replaceOne(result)}`;
+            clearValues();
+        }
+    }
+});
---
+++ src/acid-metalreaction.ts
@@ -0,0 +1,54 @@
+//HxYz + WkOHm
+const replaceOne: (str: string) => string = (str: string) => {
+    let final: string = '';
+    for (let i: number = 0; i < str.length; i++) {
+        if (str[i] !== '1') {
+            final += str[i];
+        }
+    };
+    return final;
+};
+(<HTMLButtonElement>document.getElementById('acmetalcalc')).addEventListener('click', (event) => {
+    event.preventDefault();
+    const x: number = parseFloat((<HTMLInputElement>document.getElementById('x')).value)!;
+    const y: string = ((<HTMLInputElement>document.getElementById('y')).value)!;
+    const z: number = parseFloat((<HTMLInputElement>document.getElementById('z')).value)!;
+    const m: string = ((<HTMLInputElement>document.getElementById('m')).value)!;
+    const v: number = parseFloat((<HTMLInputElement>document.getElementById('v')).value)!;
+    const clearValues: () => void = () => {
+        (<HTMLInputElement>document.getElementById('x')).value = '';
+        (<HTMLInputElement>document.getElementById('y')).value = '';
+        (<HTMLInputElement>document.getElementById('z')).value = '';
+        (<HTMLInputElement>document.getElementById('m')).value = '';
+        (<HTMLInputElement>document.getElementById('v')).value = '';
+    };
+    if (isNaN(x) || y == '' || isNaN(z) || m == '' || isNaN(v)) {
+        (<HTMLParagraphElement>document.getElementById('acmetalout')).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        clearValues();
+    } else if (x <= 0 || z <= 0 || v <= 0) {
+        (<HTMLParagraphElement>document.getElementById('acmetalout')).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        clearValues();
+    } else if (Math.floor(x) !== x || Math.floor(z) !== z || Math.floor(v) !== v) {
+        (<HTMLParagraphElement>document.getElementById('acmetalout')).innerHTML = `<p class="text-warning">All values must be integers</p>`;
+        clearValues();
+    } else if (x > 3 || v > 3) {
+        (<HTMLParagraphElement>document.getElementById('acmetalout')).innerHTML = `<p class="text-warning">The value of x or v cannot be more than 3</p>`;
+        clearValues();
+    } else if (z > 1) {
+        (<HTMLParagraphElement>document.getElementById('acmetalout')).innerHTML = `<p class="text-warning">The value of z cannot be more than 1</p>`;
+        clearValues();
+    } else if (m == "Cu" || m == "Ag" || m == "Au" || m == "Pt" || m == "cu" || m == "ag" || m == "au" || m == "pt") {
+        (<HTMLParagraphElement>document.getElementById('acmetalout')).innerHTML = `<p class="text-warning">There is not any reaction since the metal is one of these (Cu,Ag,Au,Pt)</p>`;
+        clearValues();
+    } else {
+        if (x === v) { //2 HxYz + 2 M --> 2 MY + x H2
+            const result: string = `<span class="text-warning">2</span>H<sub class="text-warning">${x}</sub><span class="text-warning">${y}</span><sub class="text-warning">${z}</sub> + <span class="text-warning">2</span><span class="text-warning">${m}</span> &#8594; <span class="text-warning">2</span><span class="text-warning">${m}</span><span class="text-warning">${y}</span> + <span class="text-warning">${x}</span>H<sub>2</sub>`;
+            (<HTMLParagraphElement>document.getElementById('acmetalout')).innerHTML = `${replaceOne(result)}`;
+            clearValues();
+        } else { //2*v HxYy + 2*x M --> 2 MxYv + v*x H2
+            const result: string = `<span class="text-warning">${2*v}</span>H<sub class="text-warning">${x}</sub><span class="text-warning">${y}</span><sub class="text-warning">${z}</sub> + <span class="text-warning">${2*x}</span><span class="text-warning">${m}</span> &#8594; <span class="text-warning">2</span><span class="text-warning">${m}</span><sub class="text-warning">${x}</sub><span class="text-warning">${y}</span><sub class="text-warning">${v}</sub> + <span class="text-warning">${x*v}</span>H<sub>2</sub>`;
+            (<HTMLParagraphElement>document.getElementById('acmetalout')).innerHTML = `${replaceOne(result)}`;
+            clearValues();
+        }
+    }
+});
