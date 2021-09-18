5f78fcb0c9b2b2d462c27fca8b8e55336ddfb27e
dk-raw <ddkatsios@protonmail.com>
2021-09-18 17:05:00 +0300

    Prod: fixed decimal values for LCM and GCD

--- dist/gcd.js
+++ dist/gcd.js
@@ -8,6 +8,18 @@ document.querySelector('#gcdcalc').addEventListener('click', (event) => {
         document.querySelector('#gcda').value = '';
         document.querySelector('#gcdb').value = '';
     }
+    else if (a !== Math.floor(a) || b !== Math.floor(b)) {
+        if (a / b === Math.floor(a / b) || b / a === Math.floor(b / a)) {
+            document.querySelector('#gcdout').innerHTML = `<p class="text-success">gcd(${a},${b}) = ${GCD(a, b)}</p>`;
+            document.querySelector('#gcda').value = '';
+            document.querySelector('#gcdb').value = '';
+        }
+        else {
+            document.querySelector('#gcdout').innerHTML = `<p class="text-warning">There is no solution for gcd(${a},${b})</p>`;
+            document.querySelector('#gcda').value = '';
+            document.querySelector('#gcdb').value = '';
+        }
+    }
     else {
         document.querySelector('#gcdout').innerHTML = `<p class="text-success">gcd(${a},${b}) = ${GCD(a, b)}</p>`;
         document.querySelector('#gcda').value = '';
