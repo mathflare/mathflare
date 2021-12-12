626ab77d7b1d7c72ca91e0c94a5b39ba900244aa
dk-raw <ddkatsios@protonmail.com>
2021-12-12 20:33:09 +0200

    refactor: upgraded weekly puzzle

--- dist/weeklypuzzle.js
+++ dist/weeklypuzzle.js
@@ -1,17 +1,52 @@
 "use strict";
-document.querySelector('#puzzlecalc').addEventListener('click', (event) => {
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+document.querySelector('#puzzlecalc').addEventListener('click', (event) => __awaiter(void 0, void 0, void 0, function* () {
     event.preventDefault();
-    const answer = parseFloat(document.querySelector('#answer').value);
-    if (isNaN(answer)) {
-        document.querySelector('#puzzleoutput').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
-    }
-    else if (answer !== 184) {
-        document.querySelector('#puzzleoutput').innerHTML = `<p class="text-warning">False! Try again!</p>`;
+    const guess = document.querySelector('#guess').value;
+    const email = document.querySelector('#email').value;
+    if (guess.length > 0 && email.length > 0 && /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+        $.ajax({
+            url: 'https://mathflarepuzzlevalidator.herokuapp.com/validate',
+            type: 'POST',
+            cache: false,
+            dataType: "json",
+            data: {
+                guess: guess,
+                userEmail: email
+            },
+            beforeSend: () => {
+                document.querySelector('#puzzlecalc').disabled = true;
+                document.querySelector('#puzzlecalc').value = 'Checking...';
+                document.querySelector('#puzzleoutput').innerHTML = '<p>Checking...</p>';
+            },
+            success: (response) => {
+                document.querySelector('#puzzlecalc').disabled = false;
+                document.querySelector('#puzzlecalc').value = 'Check your answer';
+                if (response.status === 'success') {
+                    document.querySelector('#puzzleoutput').innerHTML = `<p class="text-success">${response.message} The answer is ${response.answer}</p>`;
+                }
+                else if (response.status === 'failure') {
+                    document.querySelector('#puzzleoutput').innerHTML = `<p class="text-danger">${response.message} The answer is not ${response.answer}</p>`;
+                }
+            },
+            error: (error) => {
+                console.error(error);
+                document.querySelector('#puzzleoutput').innerHTML = `<p class="text-danger">Error! Please try again later.</p>`;
+            }
+        });
     }
     else {
-        document.querySelector('#puzzleoutput').innerHTML = `<p class="text-success">Congrats! You solved the puzzle!</p>`;
+        document.querySelector('#puzzleoutput').innerHTML = '<p class="text-warning">Please enter a valid email address and guess</p>';
     }
-});
+}));
 document.querySelector('#clearbtn').addEventListener('click', (event) => {
     event.preventDefault();
     (document.querySelector('#answer').value) = '';
--- src/weeklypuzzle.ts
+++ src/weeklypuzzle.ts
@@ -1,15 +1,38 @@
-//copyrights (c) 2021 MathFlare LLC. All rights reserved.
-(<HTMLButtonElement>document.querySelector('#puzzlecalc')).addEventListener('click', (event: Event) => {
+(<HTMLButtonElement>document.querySelector('#puzzlecalc')).addEventListener('click', async (event: Event) => {
     event.preventDefault();
-    const answer: number = parseFloat((<HTMLInputElement>document.querySelector('#answer')).value)!;
-    if (isNaN(answer)) {
-        (<HTMLParagraphElement>document.querySelector('#puzzleoutput')).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
-
-    } else if (answer !== 184) {
-        (<HTMLParagraphElement>document.querySelector('#puzzleoutput')).innerHTML = `<p class="text-warning">False! Try again!</p>`;
+    const guess: string = (<HTMLInputElement>document.querySelector('#guess')).value!;
+    const email: string = (<HTMLInputElement>document.querySelector('#email')).value!;
+    if (guess.length > 0 && email.length > 0 && /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)) {
+        $.ajax({
+            url: 'https://mathflarepuzzlevalidator.herokuapp.com/validate',
+            type: 'POST',
+            cache: false,
+            dataType: "json",
+            data: {
+                guess: guess,
+                userEmail: email
+            },
+            beforeSend: () => {
+                (<HTMLButtonElement>document.querySelector('#puzzlecalc')).disabled = true;
+                (<HTMLButtonElement>document.querySelector('#puzzlecalc')).value = 'Checking...';
+                (<HTMLParagraphElement>document.querySelector('#puzzleoutput')).innerHTML = '<p>Checking...</p>';
+            },
+            success: (response: any) => {
+                (<HTMLButtonElement>document.querySelector('#puzzlecalc')).disabled = false;
+                (<HTMLButtonElement>document.querySelector('#puzzlecalc')).value = 'Check your answer';
+                if (response.status === 'success') {
+                    (<HTMLParagraphElement>document.querySelector('#puzzleoutput')).innerHTML = `<p class="text-success">${response.message} The answer is ${response.answer}</p>`;
+                } else if (response.status === 'failure') {
+                    (<HTMLParagraphElement>document.querySelector('#puzzleoutput')).innerHTML = `<p class="text-danger">${response.message} The answer is not ${response.answer}</p>`;
+                }
+            },
+            error: (error: any) => {
+                console.error(error);
+                (<HTMLParagraphElement>document.querySelector('#puzzleoutput')).innerHTML = `<p class="text-danger">Error! Please try again later.</p>`;
+            }
+        });
     } else {
-        (<HTMLParagraphElement>document.querySelector('#puzzleoutput')).innerHTML = `<p class="text-success">Congrats! You solved the puzzle!</p>`;
-
+        (<HTMLParagraphElement>document.querySelector('#puzzleoutput')).innerHTML = '<p class="text-warning">Please enter a valid email address and guess</p>';
     }
 });
 (<HTMLButtonElement>document.querySelector('#clearbtn')).addEventListener('click', (event: Event) => {
@@ -17,4 +40,3 @@
     ((<HTMLInputElement>document.querySelector('#answer')).value) = '';
     (<HTMLParagraphElement>document.querySelector('#puzzleoutput')).innerText = '';
 });
-//copyrights (c) 2021 MathFlare LLC. All rights reserved.
