8a7ebb0ea942ca6fa7a2e7e9ac7080e6f8ec2e05
Sarmouts <saradis.m06@gmail.com>
2022-01-09 19:10:20 +0200

    fix: minor changes

--- dist/allpossibleadditions.js
+++ dist/allpossibleadditions.js
@@ -4,40 +4,56 @@ document.querySelector('#natnumbercalc').addEventListener('click', (event) => {
     const num = parseFloat(document.querySelector('#num').value);
     if (isNaN(num)) {
         document.querySelector('#tablebody').innerText = '';
-        document.querySelector('#natnumberout').innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td colspan="1">Please enter a valid value</td>`;
+        tr.classList.add('text-warning');
+        document.querySelector('#tablebody').appendChild(tr);
     }
     else if (num < 0) {
         document.querySelector('#tablebody').innerText = '';
-        document.querySelector('#natnumberout').innerHTML = `<p class="text-warning">The number must be positive</p>`;
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td colspan="1">The number must be positive</td>`;
+        tr.classList.add('text-warning');
+        document.querySelector('#tablebody').appendChild(tr);
     }
     else if (Math.floor(num) !== num) {
         document.querySelector('#tablebody').innerText = '';
-        document.querySelector('#natnumberout').innerHTML = `<p class="text-warning">The number must be an integer</p>`;
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td colspan="1">The number must be an integer</td>`;
+        tr.classList.add('text-warning');
+        document.querySelector('#tablebody').appendChild(tr);
+    }
+    else if (num > 100000) {
+        document.querySelector('#tablebody').innerText = '';
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td colspan="1">The number is too big(it must be <= 100000</td>`;
+        tr.classList.add('text-warning');
+        document.querySelector('#tablebody').appendChild(tr);
     }
     else if (num % 2 !== 0) {
         document.querySelector('#tablebody').innerText = '';
-        document.querySelector('#natnumberout').innerHTML = `<p class="text-success">All the additions for number ${num} are ${((num - 1) / 2) + 1}</p>`;
+        const possadd = ((num - 1) / 2) + 1;
+        document.querySelector('#natnumberout').innerHTML = `<p class="text-success">All the additions for number ${num} are ${possadd}</p>`;
         let x = num;
         let y = 0;
-        for (let i = 0; i < ((num - 1) / 2) + 1; i++) {
+        for (let i = 0; i < possadd; i++) {
             const tr = document.createElement('tr');
             tr.innerHTML = `<td colspan="1">${x} + ${y}</td>`;
             document.querySelector('#tablebody').appendChild(tr);
-            console.log(x, y);
             x--;
             y++;
         }
     }
     else {
         document.querySelector('#tablebody').innerText = '';
-        document.querySelector('#natnumberout').innerHTML = `<p class="text-success">All the additions for number ${num} are ${(num / 2) + 1}</p>`;
+        const possadd = ((num - 1) / 2) + 1;
+        document.querySelector('#natnumberout').innerHTML = `<p class="text-success">All the additions for number ${num} are ${possadd}</p>`;
         let x = num;
         let y = 0;
-        for (let i = 0; i < (num / 2) + 1; i++) {
+        for (let i = 0; i < possadd; i++) {
             const tr = document.createElement('tr');
             tr.innerHTML = `<td colspan="1">${x} + ${y}</td>`;
             document.querySelector('#tablebody').appendChild(tr);
-            console.log(x, y);
             x--;
             y++;
         }
--- src/allpossibleadditions.ts
+++ src/allpossibleadditions.ts
@@ -5,36 +5,54 @@
 
     if (isNaN(num)) {
         (<HTMLTableElement>document.querySelector('#tablebody')).innerText = '';
-        (<HTMLParagraphElement>document.querySelector('#natnumberout')).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
+        const tr: HTMLTableRowElement = document.createElement('tr');
+        tr.innerHTML = `<td colspan="1">Please enter a valid value</td>`;
+        tr.classList.add('text-warning');
+        (<HTMLTableElement>document.querySelector('#tablebody')).appendChild(tr);
+        //(<HTMLParagraphElement>document.querySelector('#natnumberout')).innerHTML = `<p class="text-warning">Please enter a valid value</p>`;
     } else if (num < 0) {
         (<HTMLTableElement>document.querySelector('#tablebody')).innerText = '';
-        (<HTMLParagraphElement>document.querySelector('#natnumberout')).innerHTML = `<p class="text-warning">The number must be positive</p>`;
+        const tr: HTMLTableRowElement = document.createElement('tr');
+        tr.innerHTML = `<td colspan="1">The number must be positive</td>`;
+        tr.classList.add('text-warning');
+        (<HTMLTableElement>document.querySelector('#tablebody')).appendChild(tr);
+        //(<HTMLParagraphElement>document.querySelector('#natnumberout')).innerHTML = `<p class="text-warning">The number must be positive</p>`;
     } else if (Math.floor(num) !== num) {
         (<HTMLTableElement>document.querySelector('#tablebody')).innerText = '';
-        (<HTMLParagraphElement>document.querySelector('#natnumberout')).innerHTML = `<p class="text-warning">The number must be an integer</p>`;
+        const tr: HTMLTableRowElement = document.createElement('tr');
+        tr.innerHTML = `<td colspan="1">The number must be an integer</td>`;
+        tr.classList.add('text-warning');
+        (<HTMLTableElement>document.querySelector('#tablebody')).appendChild(tr);
+        //(<HTMLParagraphElement>document.querySelector('#natnumberout')).innerHTML = `<p class="text-warning">The number must be an integer</p>`;
+    } else if (num > 100000) {
+        (<HTMLTableElement>document.querySelector('#tablebody')).innerText = '';
+        const tr: HTMLTableRowElement = document.createElement('tr');
+        tr.innerHTML = `<td colspan="1">The number is too big(it must be <= 100000</td>`;
+        tr.classList.add('text-warning');
+        (<HTMLTableElement>document.querySelector('#tablebody')).appendChild(tr);
     } else if (num % 2 !== 0) {
         (<HTMLTableElement>document.querySelector('#tablebody')).innerText = '';
-        (<HTMLParagraphElement>document.querySelector('#natnumberout')).innerHTML = `<p class="text-success">All the additions for number ${num} are ${((num - 1) / 2) + 1}</p>`;
+        const possadd: number = ((num - 1) / 2) + 1;
+        (<HTMLParagraphElement>document.querySelector('#natnumberout')).innerHTML = `<p class="text-success">All the additions for number ${num} are ${possadd}</p>`;
         let x: number = num;
         let y: number = 0;
-        for (let i = 0; i < ((num - 1) / 2) + 1; i++) {
+        for (let i = 0; i < possadd; i++) {
             const tr: HTMLTableRowElement = document.createElement('tr');
             tr.innerHTML = `<td colspan="1">${x} + ${y}</td>`;
             (<HTMLTableElement>document.querySelector('#tablebody')).appendChild(tr);
-            console.log(x, y);
             x--;
             y++;
         }
     } else {
         (<HTMLTableElement>document.querySelector('#tablebody')).innerText = '';
-        (<HTMLParagraphElement>document.querySelector('#natnumberout')).innerHTML = `<p class="text-success">All the additions for number ${num} are ${(num / 2) + 1}</p>`;
+        const possadd: number = ((num - 1) / 2) + 1;
+        (<HTMLParagraphElement>document.querySelector('#natnumberout')).innerHTML = `<p class="text-success">All the additions for number ${num} are ${possadd}</p>`;
         let x: number = num;
         let y: number = 0;
-        for (let i = 0; i < (num / 2) + 1; i++) {
+        for (let i = 0; i < possadd; i++) {
             const tr: HTMLTableRowElement = document.createElement('tr');
             tr.innerHTML = `<td colspan="1">${x} + ${y}</td>`;
             (<HTMLTableElement>document.querySelector('#tablebody')).appendChild(tr);
-            console.log(x, y);
             x--;
             y++;
         }
